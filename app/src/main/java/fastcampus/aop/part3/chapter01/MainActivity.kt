7fff3ac4ada50a3cd559e78b9a9a72fc55ffd055
kimseokjin0324 <kimseokjin0324@gmail.com>
2023-05-12 00:29:12 +0900

    Initial commit

---
+++ app/src/main/java/fastcampus/aop/part3/chapter01/MainActivity.kt
@@ -0,0 +1,11 @@
+package fastcampus.aop.part3.chapter01
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
